6d59c2ba82e63ebfcca22fe4f92db5302c85dbf6
Chris Gari <chrisgari@gari-homelab.party>
2024-09-07 00:18:50 +0000

    rm vestigial code

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
 kot_vid_url  = 'https://git.gari-homelab.party/CA_Irag/asset-dump/raw/branch/main/portfolio1/assets/video/kot.webm'
-# Read the video file
-video_bytes = video_file.read()
 
 # Display the video
 st.video(kot_vid_url)
