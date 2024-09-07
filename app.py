617f8371d7c5ef52098e77a0809b4e902d48ee99
Chris Gari <chrisgari@gari-homelab.party>
2024-09-07 00:17:46 +0000

    replaced local vid to remote link

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-video_file = open('/config/DS314/portfolio1/assets/video/kot.webm', 'rb')
-
+kot_vid_url  = 'https://git.gari-homelab.party/CA_Irag/asset-dump/raw/branch/main/portfolio1/assets/video/kot.webm'
 # Read the video file
 video_bytes = video_file.read()
 
 # Display the video
-st.video(video_bytes)
+st.video(kot_vid_url)
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
