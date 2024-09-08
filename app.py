0e3448eab0ed7dc25cf72c0fd26600131b2dfe23
Chris Gari <chrisgari@gari-homelab.party>
2024-09-08 05:18:01 +0000

    + initialization stuff

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import streamlit as st
+from initialization import * 
 
-# import os
-# os.environ["OPENAI_API_KEY"] = ""
-# os.environ["GOOGLE_API_KEY"] = ""
-# os.environ["HUGGINGFACEHUB_API_TOKEN"] = ""
 
-from langchain.llms import HuggingFaceEndpoint
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_openai import OpenAI
+st.write("""
+# Ask Apu anything
 
-llm_mistral = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2")
-llm_gemini = ChatGoogleGenerativeAI(model="gemini-pro")
-llm_openai = OpenAI(model="gpt-3.5-turbo-instruct")
+""")
+
+user_query = st.text_input("Input here")
+
+# llm_gemini_response = llm_gemini.invoke(user_query)
+
+# st.write(llm_gemini_response.content)
---
+++ initialization.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+
+from langchain.llms import HuggingFaceEndpoint
+"""
+langchain deprecation warning: 
+
+Importing LLMs from langchain is deprecated. 
+Importing from langchain will no longer be supported as of langchain==0.2.0. 
+Please import from langchain-community instead:`from langchain_community.llms import HuggingFaceEndpoint`.
+To install langchain-community run `pip install -U langchain-community`.
+"""
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import OpenAI
+
+llm_mistral = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2")
+llm_gemini = ChatGoogleGenerativeAI(model="gemini-pro")
+llm_openai = OpenAI(model="gpt-3.5-turbo-instruct")
