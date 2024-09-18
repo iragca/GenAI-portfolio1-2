e5bfc576fba7498e706e2395c494595b5e656b34
Chris Gari <chrisgari@gari-homelab.party>
2024-09-18 09:57:08 +0000

    cleaned some stuff up

--- Home.py
+++ Home.py
@@ -10,10 +10,7 @@ This project is made by Chris Irag of DS3A
 ## Generative AI
 """)
 
-# st.navigation([st.Page("Home.py")])
-
 st.page_link("Home.py", label="Home", icon="🏠")
 st.page_link("pages/Chatroom.py", label="Chatroom", icon="🤖")
-# st.page_link("pages/LLMs for Decryption.py", label="LLMs for Decryption", icon="🤖")
-# st.page_link("pages/Programming IDE with AI autocomplete.py", label="Programming IDE with AI autocomplete", icon="💻")
 st.page_link("pages/README.py", label="README", icon="☑")
+st.page_link("pages/Report a Bug.py", label="Report a Bug", icon="🐛")
