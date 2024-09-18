9b21597ec98a4e758aa11bec0728a426666c503d
Chris Gari <chrisgari@gari-homelab.party>
2024-09-18 07:52:07 +0000

    renamed

---
+++ Home.py
@@ -0,0 +1,19 @@
+from initialization import * 
+
+
+st.write(
+"""
+# Portfolio 1
+This project is made by Chris Irag of DS3A
+
+
+## Generative AI
+""")
+
+pg = st.navigation([st.Page("Home.py"), st.Page("pages/Chatroom.py"),st.Page("pages/README.py")])
+
+# st.page_link("Home.py", label="Home", icon="🏠")
+# st.page_link("pages/README.py", label="README", icon="☑")
+# st.page_link("pages/LLMs for Decryption.py", label="LLMs for Decryption", icon="🤖")
+# st.page_link("pages/Programming IDE with AI autocomplete.py", label="Programming IDE with AI autocomplete", icon="💻")
+
